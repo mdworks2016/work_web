6c6f26c1f9d17bc91a3cd884215b8cae3debcd74
T-yokouchi <mdworks2013@gmail.com>
2017-01-26 01:11:49 +0900

    何となくJavascript_62%終了

---
+++ 何となくJavascriptを書いていた人が一歩先に進むための本/nantonaku_section4.js
@@ -0,0 +1,39 @@
+
+//4-2 関数の定義方法
+///4-2-1
+///4-2-2 何と関数は変数に代入できます！
+//4-3 変数のスコープ
+///4-3-1 ややこしいぞ!グローバル変数とローカル変数
+
+function addNumber(num1,num2){
+    return num1 + num2;
+}
+
+console.log(addNumber(1,2)); //3
+
+///4-2-2 何と関数は変数に代入できます！
+var addNuber = function(num1,num2){
+    return num1 + num2;
+};
+
+////無名関数を変数に投入してから使用している
+console.log(addNumber(1,2)); ///3
+
+
+///4-3-1 ややこしいぞ!グローバル変数とローカル変数
+////グローバル変数は関数の外、ローカル変数は関数の中という認識には例外がある！
+
+var val = 'Globl!';
+
+function getVal(){
+    //なぜここの値がundefinedになっているかというと
+    //1.ローカル変数は関数全体で有効になる
+    //2.*の時点ではすでに「val」変数がローカル変数として解釈されている
+    //3.*の時点ではまだローカル変数「val」のvar命令が実行されていない
+    console.log(val); // undefined
+    var val = 'Local!';
+    return val;
+}
+
+console.log(getVal()); //Local!
+console.log(val); // Global!
