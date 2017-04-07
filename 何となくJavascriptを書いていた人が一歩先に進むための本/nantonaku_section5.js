1bb905891dabab540368953f9be43c33673ae216
T-yokouchi <mdworks2013@gmail.com>
2017-04-08 01:36:29 +0900

    何となくJavascript_88%終了

--- 何となくJavascriptを書いていた人が一歩先に進むための本/nantonaku_section5.js
+++ 何となくJavascriptを書いていた人が一歩先に進むための本/nantonaku_section5.js
@@ -15,7 +15,7 @@ var smartPhone = new Phone('smart phone');
 console.log(featurePhone.getName());
 console.log(smartPhone.getName());
 
-//5-2-2 prototypeプロパティの補完
+//5-2-1-2 prototypeプロパティの補完
 ///prototypeプロパティがなぜいいのか!
 ///Catの例だと、cat.barkを行った時点で毎回関数が生成されてしまう...
 ///Dogのようにprototypeプロパティに格納しておけば関数の宣言なし（暗黙参照）に実行結果が得られる
@@ -43,3 +43,20 @@ dog.bark();
 dog.cute();dog.cute();dog.cute();
 
 }());
+
+//5-2-2 prototypeプロパティの2つ目のメリット
+///コンストラクタでprototypeプロパティを宣言すると
+var Phone = function(name){
+    this.name = name;
+};
+
+var featurePhone = new Phone('feature phone');
+
+Phone.prototype.getName = function(){
+    return this.name;
+}
+
+var smartPhone = new Phone('smart phone');
+
+console.log(featurePhone.getName()); //feature phone
+console.log(smartPhone.getName()); //smart phone
