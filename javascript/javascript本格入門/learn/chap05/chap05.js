254ff2b0e97a974109f7d7628769ad5d8f767dc3
T-yokouchi <mdworks2013@gmail.com>
2017-05-07 19:57:04 +0900

    javascript本格入門_chap05制作中

--- javascript/javascript本格入門/learn/chap05/chap05.js
+++ javascript/javascript本格入門/learn/chap05/chap05.js
@@ -29,3 +29,84 @@ function hoge(){
 hoge.call(null); //結果：Global
 hoge.call(obj1); //結果：obj1 data
 hoge.call(obj2); //結果：obj2 data
+
+//5.2
+//コンストラクターはやめて、プロトタイプ使いましょうって話
+(function(){
+  //ここは関数
+  var Member = function(firstName,lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getName = function() {
+      return this.lastName + '' + this.firstName;
+    }
+  };
+  //ここでnewを宣言してコンストラクタ関数を呼び出している
+  //Memberは自作だが、ObjectとかNumberとかあらかじめ決められた形があれば分かりやすいかも
+  var men = new Member('小広','山田');
+  console.log('5.2復習用 コンストラクターの初期化'+men.getName()); //
+})();
+
+
+//5.2
+//5.2.5 オブジェクトリテラルでプロトタイプを定義する
+//■修正前のファイル
+(function(){
+  var Member1 = function(firstName,lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  };
+  Member1.prototype.getName = function(){
+    return this.lastName + '' +this.firstName;
+  };
+  Member1.prototype.toString = function(){
+    return this.lastName + this.firstName;
+  }
+  var mem1 = new Member1('ファーストネーム','ラストネーム');
+  console.log('5.2.5 オブジェクトリテラル宣言前'+mem1.getName());
+})();
+//■修正後のファイル
+(function(){
+  var Member1 = function(firstName,lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+  };
+  Member1.prototype ={
+    getName : function(){
+      return this.lastName + '' +this.firstName;
+    },
+    toString : function(){
+      return this.lastName + this.firstName;
+    }
+  };
+  //オブジェクトリテラルで格納した。
+  var mem1 = new Member1('ファーストネーム','ラストネーム');
+  console.log('5.2.5 オブジェクトリテラル宣言後'+mem1.getName());
+  //結果は同じだが、記述が最小限で済むようになった。
+})();
+
+//5.3 オブジェクトの継承-プロトタイプチェーン-
+//5.3.1 プロトタイプチェーンの基礎
+(function(){
+  'use strict'
+  var Animal = function(){};
+  Animal.prototype = {
+    walk: function(){
+      console.log('tokotoko');
+    }
+  };
+
+  var Dog = function(){
+    Animal.call(this);
+  };
+
+  Dog.prototype = new Animal();
+  Dog.prototype.bark = function(){
+    console.log('wanwan!');
+  }
+
+  var d = new Dog();
+  d.walk();
+  d.bark();
+
+})();
